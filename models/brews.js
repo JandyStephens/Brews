77b74249e5ef4e16f6a39ed4b93901e97f8d5036
Jandy <reverdy@protonmail.com>
2019-10-08 20:17:36 -0700

    corrected 2 errors that prevented server connection

--- models/brews.js
+++ models/brews.js
@@ -1,27 +1,27 @@
 var orm = require("../config/orm.js");
 
-var beer = {
+var beers = {
     all: function (cb) {
-        orm.all("beer", function (res) {
+        orm.all("beers", function (res) {
             cb(res);
         });
     },
     // The variables cols and vals are arrays.
     create: function (cols, vals, cb) {
-        orm.create("beer", cols, vals, function (res) {
+        orm.create("beers", cols, vals, function (res) {
             cb(res);
         });
     },
     update: function (objColVals, condition, cb) {
-        orm.update("beer", objColVals, condition, function (res) {
+        orm.update("beers", objColVals, condition, function (res) {
             cb(res);
         });
     },
     delete: function (condition, cb) {
-        orm.delete("beer", condition, function (res) {
+        orm.delete("beers", condition, function (res) {
             cb(res);
         });
     }
 };
 
-module.exports = beer;
+module.exports = beers;
