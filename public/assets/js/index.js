4733c43a0156d9c51b22acdd034b8a08c9b87d80
Jandy <reverdy@protonmail.com>
2019-10-09 20:25:52 -0700

    added delete button to clear empty glasses

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -47,15 +47,15 @@ $(function () {
     );
   });
 
-  $(".delete-cat").on("click", function (event) {
+  $(".delete-beer").on("click", function (event) {
     var id = $(this).data("id");
 
     // Send the DELETE request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/brews/" + id, {
       type: "DELETE"
     }).then(
       function () {
-        console.log("deleted cat", id);
+        console.log("deleted beer", id);
         // Reload the page to get the updated list
         location.reload();
       }
